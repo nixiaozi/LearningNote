047f8b2c05311ab2b9735cb5311737a58dc5ffbb
lee cheng <nixiaozi01@hotmail.com>
2019-04-18 22:15:11 +0800

    代码管理

--- ClassToSql/SqlPatten.cs
+++ ClassToSql/SqlPatten.cs
@@ -20,64 +20,6 @@ namespace ClassToSql
     }
 
     
-
-    /// <summary>
-    /// sql排序集合
-    /// </summary>
-    public class OrderByPattens
-    {
-        public List<OrderByPattenItem> _orderByPattens { get; set; } = new List<OrderByPattenItem>();
-
-        public OrderByPattens Add(string orderByName, OrderByType orderBy)
-        {
-            _orderByPattens.Add(new OrderByPattenItem(orderByName, orderBy));
-            return this;
-        }
-
-        public OrderByPattens AddRanges(List<string> orderByNames, OrderByType orderBy)
-        {
-            foreach(var item in orderByNames)
-            {
-                _orderByPattens.Add(new OrderByPattenItem(item, orderBy));
-            }
-            return this;
-        }
-
-        public override string ToString()
-        {
-            if (_orderByPattens.Count == 0)
-            {
-                return "  ";
-            }
-            else
-            {
-                List<string> orderStrs = new List<string>();
-                foreach(var item in _orderByPattens)
-                {
-                    orderStrs.Add(item.OrderByName + " " + item.OrderBy.GetDisplayName());
-                }
-
-                return " order by " + string.Join(",", orderStrs);
-            }
-        }
-    }
-
-    /// <summary>
-    /// sql单个排序
-    /// </summary>
-    public class OrderByPattenItem
-    {
-        public OrderByPattenItem(string orderByName, OrderByType orderBy)
-        {
-            this.OrderByName = orderByName;
-            OrderBy = orderBy;
-        }
-
-        public string OrderByName { get; set; }
-
-        public OrderByType OrderBy { get; set; }
-    }
-
     public class SqlPatten<T>
     {
         /// <summary>
@@ -96,7 +38,8 @@ namespace ClassToSql
             }
         }
 
-        public List<OrderByPattenItem> _orderByPattens { get; set; } = new List<OrderByPattenItem>();
+        //不需要排序
+        //public List<OrderByPattenItem> _orderByPattens { get; set; } = new List<OrderByPattenItem>();
 
 
         /// <summary>
--- ClassToSql/SqlString.cs
+++ ClassToSql/SqlString.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassToSql.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +8,63 @@ using System.Threading.Tasks;
 
 namespace ClassToSql
 {
-    
+    /// <summary>
+    /// sql排序集合
+    /// </summary>
+    public class OrderByPattens
+    {
+        public List<OrderByPattenItem> _orderByPattens { get; set; } = new List<OrderByPattenItem>();
+
+        public OrderByPattens Add(string orderByName, OrderByType orderBy)
+        {
+            _orderByPattens.Add(new OrderByPattenItem(orderByName, orderBy));
+            return this;
+        }
+
+        public OrderByPattens AddRanges(List<string> orderByNames, OrderByType orderBy)
+        {
+            foreach (var item in orderByNames)
+            {
+                _orderByPattens.Add(new OrderByPattenItem(item, orderBy));
+            }
+            return this;
+        }
+
+        public override string ToString()
+        {
+            if (_orderByPattens.Count == 0)
+            {
+                return "  ";
+            }
+            else
+            {
+                List<string> orderStrs = new List<string>();
+                foreach (var item in _orderByPattens)
+                {
+                    orderStrs.Add(item.OrderByName + " " + item.OrderBy.GetDisplayName());
+                }
+
+                return " order by " + string.Join(",", orderStrs);
+            }
+        }
+    }
+
+    /// <summary>
+    /// sql单个排序
+    /// </summary>
+    public class OrderByPattenItem
+    {
+        public OrderByPattenItem(string orderByName, OrderByType orderBy)
+        {
+            this.OrderByName = orderByName;
+            OrderBy = orderBy;
+        }
+
+        public string OrderByName { get; set; }
+
+        public OrderByType OrderBy { get; set; }
+    }
+
 
 
     public static class SqlString
