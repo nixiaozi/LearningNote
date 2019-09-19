298639af02d29144051dc810af29d154905b2181
nixiaozi <776493846@qq.com>
2019-09-19 18:25:33 +0800

    Add RabbitMq

---
+++ RabbitMqTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqTest
+{
+    public class Class1
+    {
+    }
+}
