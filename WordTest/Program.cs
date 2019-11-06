6bc6d4ee801ed31ca725f30a12bb6636148ff75e
lee cheng <nixiaozi01@hotmail.com>
2019-11-06 14:35:37 +0800

    添加word文档编辑

---
+++ WordTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTest
+{
+    class Program
+    {
+
+        //使用了Microsoft.Office.Interop.Word 进行word文档处理
+        static void Main(string[] args)
+        {
+            Application winword = new Application();
+
+            Document document = new Document();
+
+           
+
+        }
+    }
+}
