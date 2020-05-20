42dec01ee4cba6442c323bc00b5d6544340ab5c3
lee cheng <nixiaozi01@hotmail.com>
2020-05-21 03:22:06 +0800

    运算符重载的示例

--- ClassConvertOverLoadConsole/Program.cs
+++ ClassConvertOverLoadConsole/Program.cs
@@ -15,6 +15,31 @@ namespace ClassConvertOverLoadConsole
 
             animal.Todo();
 
+            Console.WriteLine((new Point(1, 4) + new Point(8, 5)).x);
+
         }
     }
+
+
+    class Point
+    {
+        public int x;
+        public int y;
+        
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point operator +(Point a, Point b)
+        {
+            return new Point(a.x+b.x,b.y+a.y);
+        }
+
+
+
+    }
+
+
 }
