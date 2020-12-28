607e3f81b9477af34b8edb1ef11c8bc038185a0d
lee cheng <nixiaozi01@hotmail.com>
2020-12-28 23:56:32 +0800

    VueTest 使用Vue还是最好使用VS code会方便很多

---
+++ VueLearnTest/AppTest.vue
@@ -0,0 +1,29 @@
+﻿<template>
+    <h1>Hello!</h1>
+</template>
+
+<script>
+    import Vue from "vue"; // 需要添加Vue 引用，否则会出现 new Vue() 时 'Vue' is not defined  no-undef 的错误
+    // 我们的数据对象
+    var data = { a: 1 }
+
+    // 该对象被加入到一个 Vue 实例中
+    var vm = new Vue({
+        data: data
+    })
+
+    // 获得这个实例上的 property
+    // 返回源数据中对应的字段
+    vm.a == data.a // => true
+    console.info("vm.a == data.a is:" + (vm.a == data.a));
+
+    // 设置 property 也会影响到原始数据
+    vm.a = 2
+    data.a // => 2
+    console.info("data.a is:" + data.a + ";after vm.a = 2");
+
+    // ……反之亦然
+    data.a = 3
+    vm.a // => 3
+    console.info("vm.a is:" + vm.a + "; after data.a = 3");
+</script>
---
+++ VueLearnTest/VueCli.md
@@ -0,0 +1,16 @@
+﻿# 可以使用以下方法安装Vue命令行工具
+npm install -g @vue/cli
+
+# 可以使用以下命令行添加快速原型创建工具
+npm install -g @vue/cli @vue/cli-service-global
+
+# 以下是常用命令
+vue serve   -- 使用快速原型创建的服务启动命令 它会按顺序查找当前目录下的 main.js, index.js, App.vue, app.vue 并且以它为入口进行执行
+
+vue serve MyComponent.vue  -- 使用快速原型命令直接执行 MyComponent.vue 这个文件
+
+vue build  -- 使用快速原型命令创建这个应用 具体可以参见 https://cli.vuejs.org/guide/build-targets.html
+
+# 在网上找到一个有用的 网站后台前端框架 https://www.creative-tim.com/product/vue-black-dashboard?affiliate_id=116187# 这个可以作为参考
+
+# https://cn.vuejs.org/v2/guide/computed.html   这里包含如何使用Vue和第三方 Ajax 和通用工具进行Http请求的工具。 axios  lodash
