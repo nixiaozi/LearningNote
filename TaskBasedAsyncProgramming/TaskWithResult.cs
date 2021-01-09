9039731f60990f847e9cd1d6c2847c91db327e15
lee cheng <nixiaozi01@hotmail.com>
2021-01-09 16:17:35 +0800

    添加了一个有返回值的task功能

---
+++ TaskBasedAsyncProgramming/TaskWithResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskBasedAsyncProgramming
+{
+    public  class TaskWithResult
+    {
+        public static string ReturnStrAction(CancellationToken ct)
+        {
+            bool moreToDo = true;
+            int whileTime = 0;
+            while (moreToDo&&whileTime<10)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Do ReturnStrAction");
+
+                if (ct.IsCancellationRequested)
+                {
+                    return "Get The Error";
+                    // ct.ThrowIfCancellationRequested();
+                }
+
+                whileTime++;
+            }
+
+            return "Hi, you got it!";
+        }
+
+
+    }
+}
