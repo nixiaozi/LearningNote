aaee1c167df99e4c68edea3dc2885d09244201fe
lee cheng <nixiaozi01@hotmail.com>
2021-01-18 21:08:38 +0800

    测试Thread.Sleep 是阻塞当前线程开始，只是在方法内部添加一个阻止器，结果发现是结果阻塞当前线程没有问题。

---
+++ MutilateThreadTest/TestWithWaitHold.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MutilateThreadTest
+{
+    public static class TestWithWaitHold
+    {
+        public static void HoldWait()
+        {
+            Console.WriteLine("HoldWait==>1");
+            Thread.Sleep(5000);
+            var str =  MustHoldWait();
+            Console.WriteLine("HoldWait==>2");
+
+        }
+
+
+        public static string MustHoldWait()
+        {
+            Console.WriteLine("Begin MustHold Wait");
+            Thread.Sleep(7000);
+            Console.WriteLine("MustHoldWait===>1");
+
+            return "";
+        }
+
+
+    }
+}
