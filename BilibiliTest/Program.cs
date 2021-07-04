53f13e0e17fddbb915f91bb9050a69e4d6d5c0f7
lee cheng <nixiaozi01@hotmail.com>
2021-07-05 04:18:56 +0800

    bilibili 获取av号 测试

---
+++ BilibiliTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BilibiliTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var url = "https://www.bilibili.com/video/av671737528/";
+
+            var start = url.LastIndexOf("av")+2;
+
+            var end = url.Length-1;
+
+            var length = end - start;
+
+            var result = url.Substring(start, length);
+
+
+        }
+    }
+}
